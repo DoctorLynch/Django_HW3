da4fdbd000670140ab74a3f07d6fa4f9fd7a96bc
DoctorLynch <doctor_linchenko@mail.ru>
2023-10-02 00:35:01 +0300

    HW3

--- main/management/commands/fill.py
+++ main/management/commands/fill.py
@@ -6,12 +6,18 @@ from main.models import Category
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
         category_list = [
             {'name': 'Фрукты', 'description': 'растут на деревьях', 'id': '1'},
             {'name': 'Овощи', 'description': 'растут на земле', 'id': '2'},
             {'name': 'Ягоды', 'description': 'растут и на земле, и на деревьях', 'id': '3'}
 
         ]
-
+        categories_for_create = []
         for category_item in category_list:
-            Category.objects.create(**category_item)
+            categories_for_create.append(
+                Category(**category_item)
+            )
+
+        Category.objects.bulk_create(categories_for_create)
+
